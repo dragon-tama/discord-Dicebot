d54710b1b1c4adf104218acc3aa37a268a954c5f
dragon-tama <34066491+dragon-tama@users.noreply.github.com>
2018-01-30 04:22:28 +0900

    Add files via upload

---
+++ dice1-0-1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import discord
+import random
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('----')
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('----')
+
+@client.event
+async def on_message(message):
+    
+    if message.content.startswith('!help'):
+        if client.user != message.author:
+            res = "exp.) if you send \'!1d100\'.\nthis bot return the result."
+            await client.send_message(message.channel, res)
+
+    if message.content.startswith('!'):
+        if client.user != message.author:
+            m = message.content.lstrip('!')
+            dice = m.split('d')
+            stop = int(dice[0]) * int(dice[1])
+            res = random.randrange(1, stop)
+            await client.send_message(message.channel, res)
+
+client.run("token")
