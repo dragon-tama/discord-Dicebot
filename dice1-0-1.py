a93ea2b1de6cb6a7f5e75f0e1d771cb8eba6eedc
dragon-tama <34066491+dragon-tama@users.noreply.github.com>
2018-02-06 12:02:07 +0900

    Update dice1-0-1.py
    加算に対応
    ダイスの出目を個々に表示

--- dice1-0-1.py
+++ dice1-0-1.py
@@ -2,6 +2,7 @@
 
 import discord
 import random
+import re
 import asyncio
 
 client = discord.Client()
@@ -19,15 +20,48 @@ async def on_message(message):
     
     if message.content.startswith('!help'):
         if client.user != message.author:
-            res = "e.g. if you send \'!1d100\', this bot return the result."
+            res = "e.g. if you send \'!1d100\'; \'!2D6+6\', this bot return the result."
             await client.send_message(message.channel, res)
 
     elif message.content.startswith('!'):
         if client.user != message.author:
-            m = message.content.lstrip('!')
-            dice = m.split('d')
-            stop = int(dice[0]) * int(dice[1])
-            res = random.randrange(1, stop)
-            await client.send_message(message.channel, res)
+            m = re.match(r"\!([0-9]{1,3})[Dd]([0-9]{1,3})", message.content)
+            l = re.match(r"\!([0-9]{1,3})[Dd]([0-9]{1,3})\+([0-9]{1,3})", message.content)
+
+            if l:
+                count = int(l.group(1))
+                dice = int(l.group(2))
+                add = int(l.group(3))
+                result = []
+                addresult = 0
+
+                for n in range(count):
+                    i = random.randrange(1, dice)
+                    result.append(i)
+                    addresult += i
+
+                addresult += add
+                maped = map(str, result)
+                each = ','.join(maped)
+                res = str(addresult) + " (" + each + ")"
+                await client.send_message(message.channel, res)
+
+
+
+            elif m:
+                count = int(m.group(1))
+                dice = int(m.group(2))
+                result = []
+                addresult = 0
+
+                for n in range(count):
+                    i = random.randrange(1, dice)
+                    result.append(i)
+                    addresult += i
+
+                maped = map(str, result)
+                each = ','.join(maped)
+                res = str(addresult) + " (" + each + ")"
+                await client.send_message(message.channel, res)
 
 client.run("token")
